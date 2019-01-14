241d8d267ca51c3c36631c665ec79583daaad853
小川秀哉 <shuya464924@gmail.com>
2019-01-14 20:05:20 +0900

    checkout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_12_123227) do
+ActiveRecord::Schema.define(version: 2019_01_13_111240) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_01_12_123227) do
     t.string "prefecture"
     t.string "city"
     t.string "apartment"
+    t.string "url"
   end
 
 end
